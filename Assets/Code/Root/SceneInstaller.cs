26f6acab2cd37e7547c510da4cef2f9d0acb0fc4
Smirnyashka <pasha.smirnoff.2004@gmail.com>
2024-09-08 13:59:18 +0300

    add input system

---
+++ Assets/Code/Root/SceneInstaller.cs
@@ -0,0 +1,46 @@
+﻿using Code.Configs;
+using Code.Hero;
+using Code.Services.AssetProvider;
+using Code.Services.InputService;
+using UnityEngine;
+using Zenject;
+
+namespace Code.Root
+{
+    public class SceneInstaller : MonoInstaller
+    {
+        [SerializeField]private PlayerMovement heroPlayerMovement;
+        
+        public override void InstallBindings()
+        {
+            BindConfigs();
+            BindServices();
+            BindUnits();
+            BindAnimation();
+        }
+
+        private void BindAnimation()
+        {
+            Container.Bind<Animator>().FromComponentInHierarchy().AsSingle();
+            Container.BindInterfacesAndSelfTo<AnimationController>().AsSingle();
+        }
+
+        private void BindUnits()
+        {
+            Container.Bind<HeroConfig>().AsSingle();
+            Container.BindInterfacesAndSelfTo<PlayerMovement>().FromInstance(heroPlayerMovement).AsSingle();
+            Container.Bind<IAttackble>().To(typeof(PlayerAttack)).AsSingle();
+        }
+
+        private void BindConfigs()
+        {
+            Container.Bind<AssetPathes>().AsSingle();
+        }
+
+        private void BindServices()
+        {
+            Container.Bind<GameInput>().AsSingle();
+            Container.BindInterfacesAndSelfTo<SimpleInput>().AsSingle();
+        }
+    }
+}
---
+++ Assets/Code/Services/InputService/SimpleInput.cs
@@ -0,0 +1,43 @@
+﻿using Code.Hero;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Zenject;
+
+namespace Code.Services.InputService
+{
+    public class SimpleInput: MonoBehaviour
+    {
+        private IMovable _movable;
+        private IAttackble _attackble;
+        private GameInput _gameInput;
+
+        [Inject]
+        public void Construct(IMovable movable, IAttackble attackble, GameInput gameInput)
+        {
+            _movable = movable;
+            _attackble = attackble;
+            _gameInput = gameInput;
+            _gameInput.Enable();
+        }
+
+        public void Update() => ReadInput();
+
+        private void OnEnable() => 
+            _gameInput.GamePlay.Attack.performed += OnAttackPerformed;
+
+        private void OnDisable() => 
+            _gameInput.GamePlay.Attack.performed -= OnAttackPerformed;
+
+        private void OnAttackPerformed(InputAction.CallbackContext obj) => 
+            _attackble.Attack();
+
+
+        private void ReadInput()
+        {
+            var inputDirection = _gameInput.GamePlay.Movement.ReadValue<Vector3>();
+            var direction = new Vector3(inputDirection.x, 0f, inputDirection.y);
+
+            _movable.Move(direction);
+        }
+    }
+}
